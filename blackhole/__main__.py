75bf7e29558bb93dff2cea569534e883af88e4e0
MAGNOPUSGLOBAL\andrew.bueno <andrew.bueno@magnopus.com>
2024-05-01 16:37:10 -0700

    Forgot to move 'import sys' line from __init__.py to __main__.py [VPS-1905]

--- blackhole/__main__.py
+++ blackhole/__main__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 import uvicorn
 import logging
 from blackhole.server import blackhole_api
